146b5214094320af7176df89059bd4a0b24de327
Shruti <er.shrutimishra.2002@gmail.com>
2023-09-24 12:12:40 +0530

    Fix Width issue

--- src/screen/Screen.js
+++ src/screen/Screen.js
@@ -1,20 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/navigation/Navbar'
 import Footer from '../components/navigation/Footer'
 // import { UserAuthContextProvider } from '../context/UserAuthContext'
 const Screen = (props) => {
+
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [screenHeight, setScreenHeight] = useState(window.innerHeight);
+
+  const handleResize = () => {
+    setScreenWidth(window.innerWidth);
+    setScreenHeight(window.innerHeight);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <>
+    <div style={{
+      width: `${screenWidth}px`,
+      height: `${screenHeight}px`,
+      overflowX :'hidden',
+      // border : `15px solid red`
+    }}>
       <div>
         <Navbar />
       </div>
-      <div>
-          {props?.children}
+      <div >
+        {props?.children}
       </div>
-      <div>
+      <div style={{
+        overflow :'hidden'
+      }}>
         <Footer />
       </div>
-    </>
+    </div>
   )
 }
 
